70b19ee057a76f9bc20a9e86a3a510bfdf248584
Dillon Malone <60452379+TooTechnical@users.noreply.github.com>
2026-07-16 16:38:00 +0100

    Load calendar schema in Drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "./schema";
+import * as coreSchema from "./schema";
+import * as calendarSchema from "./calendar-schema";
 import { env } from "@/lib/env";
 
 const client = postgres(env.DATABASE_URL, { prepare: false, max: 10 });
+const schema = { ...coreSchema, ...calendarSchema };
 export const db = drizzle(client, { schema });
