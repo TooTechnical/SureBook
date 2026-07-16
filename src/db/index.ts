00ce629790782d68ba8775f31dd1a6e410a4beed
Dillon Malone <60452379+TooTechnical@users.noreply.github.com>
2026-07-16 23:12:30 +0100

    Register discount redemption schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,9 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as coreSchema from "./schema";
 import * as calendarSchema from "./calendar-schema";
 import * as marketingSchema from "./marketing-schema";
+import * as discountSchema from "./discount-schema";
 import { env } from "@/lib/env";
 
 const client = postgres(env.DATABASE_URL, { prepare: false, max: 10 });
-const schema = { ...coreSchema, ...calendarSchema, ...marketingSchema };
+const schema = { ...coreSchema, ...calendarSchema, ...marketingSchema, ...discountSchema };
 export const db = drizzle(client, { schema });
