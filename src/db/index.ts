d3af1c2bcaee254663b4523b6638af284812fbb1
Dillon Malone <60452379+TooTechnical@users.noreply.github.com>
2026-07-16 23:05:47 +0100

    Register marketing schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,8 +2,9 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as coreSchema from "./schema";
 import * as calendarSchema from "./calendar-schema";
+import * as marketingSchema from "./marketing-schema";
 import { env } from "@/lib/env";
 
 const client = postgres(env.DATABASE_URL, { prepare: false, max: 10 });
-const schema = { ...coreSchema, ...calendarSchema };
+const schema = { ...coreSchema, ...calendarSchema, ...marketingSchema };
 export const db = drizzle(client, { schema });
